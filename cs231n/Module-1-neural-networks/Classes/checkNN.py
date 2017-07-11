fca0bbc84e95046ee6e6554e23d0e5dea02a0bb9
Kya <kyajmil@gmail.com>
2017-07-10 20:50:32 -0400

    fix how zeros array is declared

--- cs231n/Module-1-neural-networks/Classes/checkNN.py
+++ cs231n/Module-1-neural-networks/Classes/checkNN.py
@@ -1,10 +1,12 @@
 import numpy
 from NearestNeighbor import NearestNeighbor
 
-Xtrain = numpy.zeros(shape=(32, 32, 3)) * 50000
+Xtrain = numpy.zeros(shape=(50000, 32, 32, 3))
 Ytrain = [numpy.random.randint(0, 10)] * 50000
 
-Xtest = numpy.zeros(shape=(32, 32, 3)) * 10000
+Xtest = numpy.zeros(shape=(50000, 32, 32, 3))
 Ytest = [numpy.random.randint(0, 10)] * 10000
 nn = NearestNeighbor()
 
+Xtr_rows = Xtrain.reshape(Xtrain.shape[0], 32 * 32 * 3)
+Xte_rows = Xtest.reshape(Xtest.shape[0], 32 * 32 * 3)
