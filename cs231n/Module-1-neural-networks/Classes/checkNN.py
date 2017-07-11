b13c6a10c43f2344fbb96700f4e94669d797e795
Kya <kyajmil@gmail.com>
2017-07-10 20:40:12 -0400

    declare fake test sets and size

--- cs231n/Module-1-neural-networks/Classes/checkNN.py
+++ cs231n/Module-1-neural-networks/Classes/checkNN.py
@@ -2,6 +2,9 @@ import numpy
 from NearestNeighbor import NearestNeighbor
 
 Xtrain = numpy.zeros(shape=(32, 32, 3)) * 50000
-#Ytrain = numpy.random.randint(0, 9)
+Ytrain = [numpy.random.randint(0, 10)] * 50000
+
+Xtest = numpy.zeros(shape=(32, 32, 3)) * 10000
+Ytest = [numpy.random.randint(0, 10)] * 10000
 nn = NearestNeighbor()
 
