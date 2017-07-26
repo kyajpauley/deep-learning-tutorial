2bc5ddc27d06c7ec8eb881111a8954e8eb083548
Kya <kyajmil@gmail.com>
2017-07-25 21:52:48 -0400

    copy over and update stuff

---
+++ cs231n/Module-1-neural-networks/OptimizationExample.py
@@ -0,0 +1,28 @@
+import numpy as np
+from LossFunction import L
+
+# assume X_train is the data where each column is an example (e.g. 3073 x 50,000)
+# assume Y_train are the labels (e.g. 1D array of 50,000)
+# assume the function L evaluates the loss function
+
+X_train = [['data'] * 3073] * 50000
+Y_train = ['label'] * 50000
+
+bestloss = float("inf") # Python assigns the highest possible float value
+for num in range(1000):
+  W = np.random.randn(10, 3073) * 0.0001 # generate random parameters
+  loss = L(X_train, Y_train, W) # get the loss over the entire training set
+  if loss < bestloss: # keep track of the best solution
+    bestloss = loss
+    bestW = W
+  print('in attempt %d the loss was %f, best %f' % (num, loss, bestloss))
+
+# prints:
+# in attempt 0 the loss was 9.401632, best 9.401632
+# in attempt 1 the loss was 8.959668, best 8.959668
+# in attempt 2 the loss was 9.044034, best 8.959668
+# in attempt 3 the loss was 9.278948, best 8.959668
+# in attempt 4 the loss was 8.857370, best 8.857370
+# in attempt 5 the loss was 8.943151, best 8.857370
+# in attempt 6 the loss was 8.605604, best 8.605604
+# ... (trunctated: continues for 1000 lines)
