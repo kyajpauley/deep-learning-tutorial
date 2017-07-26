03aae6c0f4af2e1e54e9faf0102c6f80296c7b5d
Kya <kyajmil@gmail.com>
2017-07-25 22:40:45 -0400

    update stuff

--- cs231n/Module-1-neural-networks/OptimizationExample.py
+++ cs231n/Module-1-neural-networks/OptimizationExample.py
@@ -89,3 +89,36 @@ def eval_numerical_gradient(f, x):
         it.iternext()  # step to next dimension
 
     return grad
+
+
+# to use the generic code above we want a function that takes a single argument
+# (the weights in our case) so we close over X_train and Y_train
+def CIFAR10_loss_fun(W):
+    return L(X_train, Y_train, W)
+
+
+W = np.random.rand(10, 3073) * 0.001  # random weight vector
+df = eval_numerical_gradient(CIFAR10_loss_fun, W)  # get the gradient
+
+loss_original = CIFAR10_loss_fun(W)  # the original loss
+print('original loss: %f' % (loss_original,))
+
+# lets see the effect of multiple step sizes
+for step_size_log in [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1]:
+    step_size = 10 ** step_size_log
+    W_new = W - step_size * df  # new position in the weight space
+    loss_new = CIFAR10_loss_fun(W_new)
+    print('for step size %f new loss: %f' % (step_size, loss_new))
+
+    # prints:
+    # original loss: 2.200718
+    # for step size 1.000000e-10 new loss: 2.200652
+    # for step size 1.000000e-09 new loss: 2.200057
+    # for step size 1.000000e-08 new loss: 2.194116
+    # for step size 1.000000e-07 new loss: 2.135493
+    # for step size 1.000000e-06 new loss: 1.647802
+    # for step size 1.000000e-05 new loss: 2.844355
+    # for step size 1.000000e-04 new loss: 25.558142
+    # for step size 1.000000e-03 new loss: 254.086573
+    # for step size 1.000000e-02 new loss: 2539.370888
+    # for step size 1.000000e-01 new loss: 25392.214036
