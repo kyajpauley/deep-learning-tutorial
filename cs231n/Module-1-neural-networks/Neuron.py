821decb502d09d12c3fd8c0e98d9d0b528b43f6f
kyajmiller <kyajmil@gmail.com>
2017-08-02 14:51:42 -0400

    add comment

--- cs231n/Module-1-neural-networks/Neuron.py
+++ cs231n/Module-1-neural-networks/Neuron.py
@@ -5,6 +5,7 @@ import math
 class Neuron(object):
     # ...
     def forward(self, inputs):
+        # forward propagation function
         """ assume inputs and weights are 1-D numpy arrays and bias is a number """
         cell_body_sum = np.sum(inputs * self.weights) + self.bias
         firing_rate = 1.0 / (1.0 + math.exp(-cell_body_sum))  # sigmoid activation function
