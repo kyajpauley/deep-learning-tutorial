76d3cedf9eedd7c11616ca03602a805e538f95ec
kyajmiller <kyajmil@gmail.com>
2017-08-02 11:08:23 -0400

    update notes on code

--- cs231n/Module-1-neural-networks/SigmoidFunction.py
+++ cs231n/Module-1-neural-networks/SigmoidFunction.py
@@ -1,13 +1,16 @@
-w = [2,-3,-3] # assume some random weights and data
-x = [-1, -2]
+import math
+
+wVals = [2, -3, -3] # assume some random weights and data
+xVals = [-1, -2]
 
 # forward pass
-dot = w[0]*x[0] + w[1]*x[1] + w[2]  # this is a really weird way to get the dot product of something, but it
+x = wVals[0] * xVals[0] + wVals[1] * xVals[1] + wVals[2]  # this is a really weird way to get the dot product of something, but it
                                     # at least shows the math?
-f = 1.0 / (1 + math.exp(-dot)) # sigmoid function
+f = 1.0 / (1 + math.exp(-x)) # sigmoid function
+# f = 1/(1 + e**−x)
 
 # backward pass through the neuron (backpropagation)
 ddot = (1 - f) * f # gradient on dot variable, using the sigmoid gradient derivation
-dx = [w[0] * ddot, w[1] * ddot] # backprop into x
-dw = [x[0] * ddot, x[1] * ddot, 1.0 * ddot] # backprop into w
+dx = [wVals[0] * ddot, wVals[1] * ddot] # backprop into x
+dw = [xVals[0] * ddot, xVals[1] * ddot, 1.0 * ddot] # backprop into w
 # we're done! we have the gradients on the inputs to the circuit
