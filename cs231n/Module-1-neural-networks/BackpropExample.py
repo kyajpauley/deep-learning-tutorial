8866e56da3cf3af8ddf770d1912a7b43d33e9888
kyajmiller <kyajmil@gmail.com>
2017-08-02 11:16:57 -0400

    copy over code notes, need to refactor for better variable names and notes

---
+++ cs231n/Module-1-neural-networks/BackpropExample.py
@@ -0,0 +1,40 @@
+import math
+
+# fake function
+# f(x,y) = (x + σ(y))/(σ(x) + (x + y)**2)
+
+x = 3 # example values
+y = -4
+
+# forward pass
+sigy = 1.0 / (1 + math.exp(-y)) # sigmoid in numerator   #(1)
+num = x + sigy # numerator                               #(2)
+sigx = 1.0 / (1 + math.exp(-x)) # sigmoid in denominator #(3)
+xpy = x + y                                              #(4)
+xpysqr = xpy**2                                          #(5)
+den = sigx + xpysqr # denominator                        #(6)
+invden = 1.0 / den                                       #(7)
+f = num * invden # done!                                 #(8)
+
+# work backwards through broken down steps to find derivatives
+# backprop f = num * invden
+dnum = invden # gradient on numerator                             #(8)
+dinvden = num                                                     #(8)
+# backprop invden = 1.0 / den
+dden = (-1.0 / (den**2)) * dinvden                                #(7)
+# backprop den = sigx + xpysqr
+dsigx = (1) * dden                                                #(6)
+dxpysqr = (1) * dden                                              #(6)
+# backprop xpysqr = xpy**2
+dxpy = (2 * xpy) * dxpysqr                                        #(5)
+# backprop xpy = x + y
+dx = (1) * dxpy                                                   #(4)
+dy = (1) * dxpy                                                   #(4)
+# backprop sigx = 1.0 / (1 + math.exp(-x))
+dx += ((1 - sigx) * sigx) * dsigx # Notice += !! See notes below  #(3)
+# backprop num = x + sigy
+dx += (1) * dnum                                                  #(2)
+dsigy = (1) * dnum                                                #(2)
+# backprop sigy = 1.0 / (1 + math.exp(-y))
+dy += ((1 - sigy) * sigy) * dsigy                                 #(1)
+# done! phew
