409fb8131ef23790813367fc0d7a8817b04e0247
Kya <kyajmil@gmail.com>
2017-09-26 21:37:51 -0400

    tutorial to initialize sequential model

---
+++ keras/SequentialModelInit.py
@@ -0,0 +1,22 @@
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
+# declare model by passing list of layer instances
+model = Sequential([
+    Dense(32, input_shape=(784,)),
+    Activation('relu'),
+    Dense(10),
+    Activation('softmax'),
+])
+
+# or declare model and add layers after init
+model = Sequential()
+model.add(Dense(32, input_dim=784))
+model.add(Activation('relu'))
+
+# input shape specification (following snippets are equivalent for 2d layers)
+model = Sequential()
+model.add(Dense(32, input_shape=(784,)))  # pass tuple of shape
+
+model = Sequential()
+model.add(Dense(32, input_dim=784))
